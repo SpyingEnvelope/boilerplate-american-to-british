5f1a61412b13d85f17495e092d76822aa8c95b74
SpyingEnvelope <gadi800@gmail.com>
2022-03-08 06:46:03 -0700

    api works for translating singular words at the moment. It does not work for time changes or spaced words

--- components/translator.js
+++ components/translator.js
@@ -1,9 +1,46 @@
 const americanOnly = require('./american-only.js');
 const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require('./british-only.js');
+const britishToAmericanOnly = require('./british-to-american-only-spelling.js');
+const britishToAmericanSpelling = require('./british-to-american-spelling.js');
+const britishToAmericanTitles = require('./british-to-american-titles.js');
 
 class Translator {
+
+    americanToBritish (text) {
+        let textArr = text.split(" ");
+
+        for (let i = 0; i < textArr.length; i++) {
+            if (americanOnly[textArr[i]]) {
+                textArr[i] = '<span class="highlight">' + americanOnly[textArr[i]] + '</span>';
+            } else if (americanToBritishSpelling[textArr[i]]) {
+                textArr[i] = '<span class="highlight">' + americanToBritishSpelling[textArr[i]] + '</span>';
+            } else if (americanToBritishTitles[textArr[i]]) {
+                textArr[i] = '<span class="highlight">' + americanToBritishTitles[textArr[i]] + '</span>'
+            }
+        }
+
+        return textArr.join(" ");
+    }
+
+    britishToAmerican (text) {
+        let textArr = text.split(" ");
+
+        for (let i = 0; i < textArr.length; i++) {
+            if (britishOnly[textArr[i]]) {
+                textArr[i] = '<span class="highlight">' + britishOnly[textArr[i]] + '</span>';
+            } else if (britishToAmericanOnly[textArr[i]]) {
+                textArr[i] = '<span class="highlight">' + britishToAmericanOnly[textArr[i]] + '</span>';
+            } else if (britishToAmericanSpelling[textArr[i]]) {
+                textArr[i] = '<span class="highlight">' + britishToAmericanSpelling[textArr[i]] + '</span>';
+            } else if (britishToAmericanTitles[textArr[i]]) {
+                textArr[i] = '<span class="highlight">' + britishToAmericanTitles[textArr[i]] + '</span>';
+            }
+        }
+
+        return textArr.join(" ");
+    }
 
 }
 
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,22 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      console.log(req.body);
+      let translation;
+      if (req.body.locale == "american-to-british") {
+        translation = translator.americanToBritish(req.body.text);
+      }
+
+      if (req.body.locale == "british-to-american") {
+        translation = translator.britishToAmerican(req.body.text);
+      }
+
+      console.log(translation);
+
+      if (translation == req.body.text) {
+        return (res.json({'submitted': req.body.text, 'translation': 'Everything looks good to me!'}))
+      } else {
+        res.json({'submitted': req.body.text, 'translation': translation})
+      }
     });
 };
