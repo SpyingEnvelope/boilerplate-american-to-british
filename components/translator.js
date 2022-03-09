f817f8f2a1a89a7de7a478b4dd980b41b249d406
SpyingEnvelope <gadi800@gmail.com>
2022-03-09 05:59:06 -0700

    added all logic for the webapp. Seems to function as intended

--- components/translator.js
+++ components/translator.js
@@ -5,6 +5,9 @@ const britishOnly = require('./british-only.js');
 const britishToAmericanOnly = require('./british-to-american-only-spelling.js');
 const britishToAmericanSpelling = require('./british-to-american-spelling.js');
 const britishToAmericanTitles = require('./british-to-american-titles.js');
+const americanToBritishRegex = /[0-9][0-9]:[0-9][0-9]/
+const britishToAmericanRegex = /[0-9][0-9]\.[0-9][0-9]/
+const letterRegex = /[a-z]/
 
 class Translator {
 
@@ -18,14 +21,27 @@ class Translator {
                 textArr[i] = '<span class="highlight">' + americanToBritishSpelling[textArr[i]] + '</span>';
             } else if (americanToBritishTitles[textArr[i]]) {
                 textArr[i] = '<span class="highlight">' + americanToBritishTitles[textArr[i]] + '</span>'
+            } else if (americanToBritishRegex.test(textArr[i])) {
+                textArr[i] = '<span class="highlight">' + textArr[i][0] + textArr[i][1] + '.' + textArr[i][3] + textArr[i][4] + '</span>';
             }
         }
+
+        //Turn the first word into uppercase
+        if (letterRegex.test(textArr[0])){
+            let splitStr = textArr[0].split("");
+            splitStr[0] = splitStr[0].toUpperCase();
+            splitStr = splitStr.join("");
+            textArr[0] = splitStr;
+        }
+ 
 
         return textArr.join(" ");
     }
 
     britishToAmerican (text) {
-        let textArr = text.split(" ");
+        let lowerCaseText = text.toLowerCase();
+
+        let textArr = lowerCaseText.split(" ");
 
         for (let i = 0; i < textArr.length; i++) {
             if (britishOnly[textArr[i]]) {
@@ -36,8 +52,21 @@ class Translator {
                 textArr[i] = '<span class="highlight">' + britishToAmericanSpelling[textArr[i]] + '</span>';
             } else if (britishToAmericanTitles[textArr[i]]) {
                 textArr[i] = '<span class="highlight">' + britishToAmericanTitles[textArr[i]] + '</span>';
+            } else if (britishToAmericanRegex.test(textArr[i])) {
+                textArr[i] = '<span class="highlight">' + textArr[i][0] + textArr[i][1] + ':' + textArr[i][3] + textArr[i][4] + '</span>';
             }
         }
+
+        //Turn the first word into uppercase
+        if (letterRegex.test(textArr[0])){
+            let splitStr = textArr[0].split("");
+            splitStr[0] = splitStr[0].toUpperCase();
+            splitStr = splitStr.join("");
+            textArr[0] = splitStr;
+        }
+
+        
+
 
         return textArr.join(" ");
     }
--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { redundancy } = require('../components/british-to-american-only-spelling.js');
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
@@ -10,20 +11,29 @@ module.exports = function (app) {
     .post((req, res) => {
       console.log(req.body);
       let translation;
-      if (req.body.locale == "american-to-british") {
-        translation = translator.americanToBritish(req.body.text);
-      }
 
-      if (req.body.locale == "british-to-american") {
-        translation = translator.britishToAmerican(req.body.text);
-      }
-
-      console.log(translation);
-
-      if (translation == req.body.text) {
-        return (res.json({'submitted': req.body.text, 'translation': 'Everything looks good to me!'}))
-      } else {
-        res.json({'submitted': req.body.text, 'translation': translation})
+      if (!req.body.locale) {
+        res.json({'error': 'Required field(s) missing'})
+      } else if (!req.body.text) {
+        res.json({'error': 'No text to translate'})
+      } else if (req.body.locale != "american-to-british" && req.body.locale != "british-to-american") {
+        res.json({'error': 'Invalid value for locale field'});
+      } else if (req.body.text && req.body.locale) {
+        if (req.body.locale == "american-to-british") {
+          translation = translator.americanToBritish(req.body.text);
+        }
+  
+        if (req.body.locale == "british-to-american") {
+          translation = translator.britishToAmerican(req.body.text);
+        }
+  
+        console.log(translation);
+  
+        if (translation == req.body.text) {
+          return (res.json({'submitted': req.body.text, 'translation': 'Everything looks good to me!'}))
+        } else {
+          res.json({'submitted': req.body.text, 'translation': translation})
+        }
       }
     });
 };
