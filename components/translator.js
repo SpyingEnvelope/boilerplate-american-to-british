631024bf302a78d0bfbe75fa4321611e1ce14d89
SpyingEnvelope <gadi800@gmail.com>
2022-03-10 06:22:11 -0700

    added logic to pass all unit tests. All 24 unit tests are passing. functional tests need to be written

--- components/translator.js
+++ components/translator.js
@@ -6,7 +6,8 @@ const britishToAmericanOnly = require('./british-to-american-only-spelling.js');
 const britishToAmericanSpelling = require('./british-to-american-spelling.js');
 const britishToAmericanTitles = require('./british-to-american-titles.js');
 const americanToBritishRegex = /[0-9][0-9]:[0-9][0-9]/
-const britishToAmericanRegex = /[0-9][0-9]\.[0-9][0-9]/
+const britishToAmericanRegex = /[0-9][0-9]/
+const digitRegex = /[0-9]/
 const letterRegex = /[a-z]/
 const splitRegex = /\s|(?=\.)|(?=\?)|(?=\,)|(?=\!)/
 const specialRegex = /^\.|\,|\?|\!$/
@@ -16,9 +17,15 @@ class Translator {
     americanToBritish (text) {
         let textArr = text.split(splitRegex);
 
+        
         for (let i = 0; i < textArr.length; i++) {
-
-            if (americanOnly[textArr[i].toLowerCase()]) {
+            if (i + 3 <= textArr.length && americanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase() + ' ' + textArr[i + 2].toLowerCase()]) {
+                textArr[i] = '<span class="highlight">' + americanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase() + ' ' + textArr[i + 2].toLowerCase()] + '</span>';
+                textArr.splice(i + 1, 2);
+            } else if (i + 2 <= textArr.length && americanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase()]) {
+                textArr[i] = '<span class="highlight">' + americanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase()] + '</span>';
+                textArr.splice(i + 1, 1);
+            } else if (americanOnly[textArr[i].toLowerCase()]) {
                 textArr[i] = '<span class="highlight">' + americanOnly[textArr[i].toLowerCase()] + '</span>';
             } else if (americanToBritishSpelling[textArr[i].toLowerCase()]) {
                 textArr[i] = '<span class="highlight">' + americanToBritishSpelling[textArr[i].toLowerCase()] + '</span>';
@@ -26,19 +33,43 @@ class Translator {
                 textArr[i] = '<span class="highlight">' + americanToBritishTitles[textArr[i].toLowerCase()] + '</span>'
             } else if (americanToBritishRegex.test(textArr[i])) {
                 textArr[i] = '<span class="highlight">' + textArr[i][0] + textArr[i][1] + '.' + textArr[i][3] + textArr[i][4] + '</span>';
-            } else if (i == textArr.length - 1) {
-                continue;
-            } else if (americanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase()]) {
-                textArr[i] = '<span class="highlight">' + americanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase()] + '</span>';
-                textArr.splice(i + 1, 1);
-            } else if (textArr[i].toLowerCase() == 'rube' && textArr[i + 1].toLowerCase() == "goldberg") {
+            }  else if (textArr[i].toLowerCase() == 'rube' && textArr[i + 1].toLowerCase() == "goldberg") {
                 textArr[i] = '<span class="highlight">' + americanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase() + ' ' + textArr[i + 2].toLowerCase()] + '</span>';
                 textArr.splice(i + 1, 2);
             } else if (textArr[i].toLowerCase() == "mr" && textArr[i + 1] == '.' || textArr[i].toLowerCase() == "ms" && textArr[i + 1] == '.' || textArr[i].toLowerCase() == "mrs" && textArr[i + 1] == '.' || textArr[i].toLowerCase() == "mx" && textArr[i + 1] == '.' || textArr[i].toLowerCase() == "dr" && textArr[i + 1] == '.' || textArr[i].toLowerCase() == "prof" && textArr[i + 1] == '.') {
                 textArr[i] = '<span class="highlight">' + americanToBritishTitles[textArr[i].toLowerCase() + textArr[i + 1].toLowerCase()] + '</span>';
                 textArr.splice(i + 1, 1);
-            }
+            } 
         }
+
+        // for (let i = 0; i < textArr.length; i++) {
+
+        //     if (americanOnly[textArr[i].toLowerCase()]) {
+        //         textArr[i] = '<span class="highlight">' + americanOnly[textArr[i].toLowerCase()] + '</span>';
+        //     } else if (americanToBritishSpelling[textArr[i].toLowerCase()]) {
+        //         textArr[i] = '<span class="highlight">' + americanToBritishSpelling[textArr[i].toLowerCase()] + '</span>';
+        //     } else if (americanToBritishTitles[textArr[i].toLowerCase()]) {
+        //         textArr[i] = '<span class="highlight">' + americanToBritishTitles[textArr[i].toLowerCase()] + '</span>'
+        //     } else if (americanToBritishRegex.test(textArr[i])) {
+        //         textArr[i] = '<span class="highlight">' + textArr[i][0] + textArr[i][1] + '.' + textArr[i][3] + textArr[i][4] + '</span>';
+        //     } else if (i == textArr.length - 1) {
+        //         continue;
+        //     } else if (americanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase()]) {
+        //         textArr[i] = '<span class="highlight">' + americanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase()] + '</span>';
+        //         textArr.splice(i + 1, 1);
+        //     } else if (textArr[i].toLowerCase() == 'rube' && textArr[i + 1].toLowerCase() == "goldberg") {
+        //         textArr[i] = '<span class="highlight">' + americanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase() + ' ' + textArr[i + 2].toLowerCase()] + '</span>';
+        //         textArr.splice(i + 1, 2);
+        //     } else if (textArr[i].toLowerCase() == "mr" && textArr[i + 1] == '.' || textArr[i].toLowerCase() == "ms" && textArr[i + 1] == '.' || textArr[i].toLowerCase() == "mrs" && textArr[i + 1] == '.' || textArr[i].toLowerCase() == "mx" && textArr[i + 1] == '.' || textArr[i].toLowerCase() == "dr" && textArr[i + 1] == '.' || textArr[i].toLowerCase() == "prof" && textArr[i + 1] == '.') {
+        //         textArr[i] = '<span class="highlight">' + americanToBritishTitles[textArr[i].toLowerCase() + textArr[i + 1].toLowerCase()] + '</span>';
+        //         textArr.splice(i + 1, 1);
+        //     } else if (i + 1 == textArr.length - 1) {
+        //         continue;
+        //     } else if (americanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase() + ' ' + textArr[i + 2].toLowerCase()]) {
+        //         textArr[i] = '<span class="highlight">' + americanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase() + ' ' + textArr[i + 2].toLowerCase()] + '</span>';
+        //         textArr.splice(i + 1, 2);
+        //     }
+        // }
 
         //Turn the first word into uppercase
         if (letterRegex.test(textArr[0])){
@@ -67,7 +98,22 @@ class Translator {
         let textArr = text.split(splitRegex);
 
         for (let i = 0; i < textArr.length; i++) {
-            if (britishOnly[textArr[i].toLowerCase()]) {
+             if (i + 3 <= textArr.length && britishOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase() + ' ' + textArr[i + 2].toLowerCase()]) {
+                textArr[i] = '<span class="highlight">' + britishOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase() + ' ' + textArr[i + 2].toLowerCase()] + '</span>';
+                textArr.splice(i + 1, 2);
+            } else if (i + 3 <= textArr.length && britishToAmericanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase() + ' ' + textArr[i + 2].toLowerCase()]) {
+                textArr[i] = '<span class="highlight">' + britishToAmericanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase() + ' ' + textArr[i + 2].toLowerCase()] + '</span>';
+                textArr.splice(i + 1, 2);
+            } else if (i + 2 <= textArr.length && britishOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase()]) {
+                textArr[i] = '<span class="highlight">' + britishOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase()] + '</span>';
+                textArr.splice(i + 1, 1);
+            } else if (i + 2 <= textArr.length && britishToAmericanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase()]) {
+                textArr[i] = '<span class="highlight">' + britishToAmericanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase()] + '</span>';
+                textArr.splice(i + 1, 1);
+            } else if (textArr[i].toLowerCase() == 'heath' && textArr[i + 1].toLowerCase() == "robinson" && textArr[i + 2].toLowerCase() == 'device') {
+                textArr[i] = '<span class="highlight">' + britishOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase() + ' ' + textArr[i + 2].toLowerCase()] + '</span>';
+                textArr.splice(i + 1, 2);
+            } else if (britishOnly[textArr[i].toLowerCase()]) {
                 textArr[i] = '<span class="highlight">' + britishOnly[textArr[i].toLowerCase()] + '</span>';
             } else if (britishToAmericanOnly[textArr[i].toLowerCase()]) {
                 textArr[i] = '<span class="highlight">' + britishToAmericanOnly[textArr[i].toLowerCase()] + '</span>';
@@ -75,21 +121,44 @@ class Translator {
                 textArr[i] = '<span class="highlight">' + britishToAmericanSpelling[textArr[i].toLowerCase()] + '</span>';
             } else if (britishToAmericanTitles[textArr[i].toLowerCase()]) {
                 textArr[i] = '<span class="highlight">' + britishToAmericanTitles[textArr[i].toLowerCase()] + '</span>';
-            } else if (britishToAmericanRegex.test(textArr[i])) {
-                textArr[i] = '<span class="highlight">' + textArr[i][0] + textArr[i][1] + ':' + textArr[i][3] + textArr[i][4] + '</span>';
-            } else if (i == textArr.length - 1) {
-                continue;
-            } else if (britishOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase()]) {
-                textArr[i] = '<span class="highlight">' + britishOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase()] + '</span>';
+            } else if (i + 2 <= textArr.length && britishToAmericanRegex.test(textArr[i]) && textArr[i + 1][0] == '.' && britishToAmericanRegex.test(textArr[i + 1][1] + textArr[i + 1][2]) || i + 2 <= textArr.length && digitRegex.test(textArr[i]) && textArr[i + 1][0] == '.' && britishToAmericanRegex.test(textArr[i + 1][1] + textArr[i + 1][2])) {
+                textArr[i] = '<span class="highlight">' + textArr[i] + ':' + textArr[i + 1][1] + textArr[i + 1][2] + '</span>';
                 textArr.splice(i + 1, 1);
-            } else if (britishToAmericanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase()]) {
-                textArr[i] = '<span class="highlight">' + britishToAmericanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase()] + '</span>';
-                textArr.splice(i + 1, 1);
-            } else if (textArr[i].toLowerCase() == 'heath' && textArr[i + 1].toLowerCase() == "robinson" && textArr[i + 2].toLowerCase() == 'device') {
-                textArr[i] = '<span class="highlight">' + britishOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase() + ' ' + textArr[i + 2].toLowerCase()] + '</span>';
-                textArr.splice(i + 1, 2);
-            }
+            } 
         }
+
+        // for (let i = 0; i < textArr.length; i++) {
+        //  if (britishOnly[textArr[i].toLowerCase()]) {
+        //         textArr[i] = '<span class="highlight">' + britishOnly[textArr[i].toLowerCase()] + '</span>';
+        //     } else if (britishToAmericanOnly[textArr[i].toLowerCase()]) {
+        //         textArr[i] = '<span class="highlight">' + britishToAmericanOnly[textArr[i].toLowerCase()] + '</span>';
+        //     } else if (britishToAmericanSpelling[textArr[i].toLowerCase()]) {
+        //         textArr[i] = '<span class="highlight">' + britishToAmericanSpelling[textArr[i].toLowerCase()] + '</span>';
+        //     } else if (britishToAmericanTitles[textArr[i].toLowerCase()]) {
+        //         textArr[i] = '<span class="highlight">' + britishToAmericanTitles[textArr[i].toLowerCase()] + '</span>';
+        //     } else if (britishToAmericanRegex.test(textArr[i])) {
+        //         textArr[i] = '<span class="highlight">' + textArr[i][0] + textArr[i][1] + ':' + textArr[i][3] + textArr[i][4] + '</span>';
+        //     } else if (i == textArr.length - 1) {
+        //         continue;
+        //     } else if (britishOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase()]) {
+        //         textArr[i] = '<span class="highlight">' + britishOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase()] + '</span>';
+        //         textArr.splice(i + 1, 1);
+        //     } else if (britishToAmericanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase()]) {
+        //         textArr[i] = '<span class="highlight">' + britishToAmericanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase()] + '</span>';
+        //         textArr.splice(i + 1, 1);
+        //     } else if (textArr[i].toLowerCase() == 'heath' && textArr[i + 1].toLowerCase() == "robinson" && textArr[i + 2].toLowerCase() == 'device') {
+        //         textArr[i] = '<span class="highlight">' + britishOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase() + ' ' + textArr[i + 2].toLowerCase()] + '</span>';
+        //         textArr.splice(i + 1, 2);
+        //     } else if (i + 1 == textArr.length - 1) {
+        //         continue;
+        //     } else if (britishOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase() + ' ' + textArr[i + 2].toLowerCase()]) {
+        //         textArr[i] = '<span class="highlight">' + britishOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase() + ' ' + textArr[i + 2].toLowerCase()] + '</span>';
+        //         textArr.splice(i + 1, 2);
+        //     } else if (britishToAmericanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase() + ' ' + textArr[i + 2].toLowerCase()]) {
+        //         textArr[i] = '<span class="highlight">' + britishToAmericanOnly[textArr[i].toLowerCase() + ' ' + textArr[i + 1].toLowerCase() + ' ' + textArr[i + 2].toLowerCase()] + '</span>';
+        //         textArr.splice(i + 1, 2);
+        //     }
+        // }
 
         //Turn the first word into uppercase
         if (letterRegex.test(textArr[0])){
